2ef1f586008de90a4ca47791519995d0a05e1ed0
Ethan Sheynblat <ethansheynblat@Ethans-MacBook-Pro.local>
2021-04-08 14:27:45 -0700

    add a script that computes lightyear

---
+++ inc02b.R
@@ -0,0 +1,7 @@
+c <- 299792458
+lightMinute <- c * 60
+lightHour <- 60 * lightMinute
+lightDay <- 24 * lightHour
+lightYear <- 365.2422 * lightDay
+cat("length of a light year:",lightYear, "\n", sep = " ") #"\n" means line break
+cat("Thats too long")
